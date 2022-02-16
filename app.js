156f4dcbd373ec9d6b0e4b52bf82467d9d3d773a
devilmamuncn@gmail.com <mmhutsobe01@gmail.com>
2022-02-16 14:25:52 +0600

    quantity btn handler added

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-console.log('I am in a separate file')
+function updateProductNumber(product, isIncreaseing) {
+    const productInput = document.getElementById(product + '-quantity');
+    let productNumber = productInput.value;
+    if (isIncreaseing == true) {
+        productNumber = parseInt(productNumber) + 1;
+    }
+    else if (productNumber > 0) {
+        productNumber = parseInt(productNumber) - 1;
+    }
+    productInput.value = productNumber;
+
+};
+
+// phone quantity
+document.getElementById('phone-plus').addEventListener('click', function () {
+    updateProductNumber('phone', true);
+});
+
+document.getElementById('phone-minus').addEventListener('click', function () {
+    updateProductNumber('phone', false);
+});
+
+// case quantity
+document.getElementById('case-plus').addEventListener('click', function () {
+    updateProductNumber('case', true);
+});
+
+document.getElementById('case-minus').addEventListener('click', function () {
+    updateProductNumber('case', false);
+});
+
+
+
