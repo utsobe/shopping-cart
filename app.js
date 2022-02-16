d6b5769b2acc715e3ca1f86ad439646502be1d7a
devilmamuncn@gmail.com <mmhutsobe01@gmail.com>
2022-02-16 15:35:15 +0600

    product total added

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-function updateProductNumber(product, isIncreaseing) {
+// quantity increasing-decreasing function
+function updateProductNumber(product, price, isIncreaseing) {
     const productInput = document.getElementById(product + '-quantity');
     let productNumber = productInput.value;
     if (isIncreaseing == true) {
@@ -8,25 +9,50 @@ function updateProductNumber(product, isIncreaseing) {
         productNumber = parseInt(productNumber) - 1;
     }
     productInput.value = productNumber;
-
+    // product total 
+    const productTotal = document.getElementById(product + '-total');
+    productTotal.innerText = productNumber * price;
+    updateTotalPrice()
 };
 
-// phone quantity
+// getting product quantity 
+function getInputValues(product) {
+    const productInput = document.getElementById(product + '-quantity');
+    const productNumber = parseInt(productInput.value);
+    return productNumber;
+};
+
+//update total price
+function updateTotalPrice() {
+    const phoneTotal = getInputValues('phone') * 1219;
+    const caseTotal = getInputValues('case') * 59;
+    const subTotal = phoneTotal + caseTotal;
+    const tax = subTotal / 10;
+    const total = subTotal + tax;
+    // update total in html 
+    document.getElementById('sub-total').innerText = subTotal;
+    document.getElementById('tax').innerText = tax;
+    document.getElementById('total').innerText = total;
+};
+
+// phone quantity increasing event
 document.getElementById('phone-plus').addEventListener('click', function () {
-    updateProductNumber('phone', true);
+    updateProductNumber('phone', 1219, true);
 });
 
+// phone quantity decreasing event
 document.getElementById('phone-minus').addEventListener('click', function () {
-    updateProductNumber('phone', false);
+    updateProductNumber('phone', 1219, false);
 });
 
-// case quantity
+// case quantity increasing event
 document.getElementById('case-plus').addEventListener('click', function () {
-    updateProductNumber('case', true);
+    updateProductNumber('case', 59, true);
 });
 
+// case quantity decreasing event
 document.getElementById('case-minus').addEventListener('click', function () {
-    updateProductNumber('case', false);
+    updateProductNumber('case', 59, false);
 });
 
 
